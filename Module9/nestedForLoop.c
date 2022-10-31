b7069cca902d4375a28aaa0cb6e0202a708b0fde
shahidullah piaro <shahidullah.piyaro@gmail.com>
2022-10-31 07:59:00 +0600

    Nested Loop Update

--- Module9/nestedForLoop.c
+++ Module9/nestedForLoop.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int M;
+    printf("Enter the value of M: ");
+    scanf("%d",&M);
+    int i, j;
 
+    for(i=0;i<M;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+            printf("*");
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
